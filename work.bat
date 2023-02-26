d727494c6181ebb89ce28625b5f00aa39f8c2950
gleevids4 <126392014+gleevids4@users.noreply.github.com>
2023-02-26 16:44:57 +0500

    Create work.bat

---
+++ work.bat
@@ -0,0 +1,22 @@
+@echo off
+
+REM Download ngrok
+powershell -Command "Invoke-WebRequest https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-windows-amd64.zip -OutFile ngrok.zip"
+
+REM Extract ngrok
+powershell -Command "Expand-Archive ngrok.zip"
+
+REM Authenticate ngrok
+set NGROK_AUTH_TOKEN=2MGuYMvTqVrCmNlISqb5GJVeUck_6rp1ke5YLcKUiaHkCFMx8
+.\ngrok\ngrok.exe authtoken %NGROK_AUTH_TOKEN%
+
+REM Enable remote desktop access
+REG ADD "HKLM\System\CurrentControlSet\Control\Terminal Server" /v "fDenyTSConnections" /t REG_DWORD /d 0 /f
+netsh advfirewall firewall set rule group="Remote Desktop" new enable=Yes
+REG ADD "HKLM\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" /v "UserAuthentication" /t REG_DWORD /d 1 /f
+
+REM Create user account
+net user binni binnib /add
+
+REM Create tunnel
+.\ngrok\ngrok.exe tcp 3389
